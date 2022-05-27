949adf75438150949c0b1a779465986a15298ab0
Steven Jesus Delgado Benavides <delgado.steven.1997@gmail.com>
2022-05-27 11:46:13 +0200

    foto 1

---
+++ src/principal/Principal.java
@@ -0,0 +1,16 @@
+package principal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+    }
+
+    public double sumar(double a, double b) {
+        return 1.0;
+    }
+
+    public double multiplicar(double a, double b) {
+        return 1.0;
+    }
+}
